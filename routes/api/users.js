78c3686895557978a90874a322a0c4df7fa6db87
Sam <samtaylor720@gmail.com>
2018-04-30 17:34:29 -0400

    Comment

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,16 +10,17 @@ const passport = require("passport");
 //Load User model
 const User = require("../../models/User");
 
+// Responds to "/test" on page with "User Works!"
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-
 router.get("/test", (req, res) => res.json({ msg: "Users Works!" }));
 
+
+// This is one of the endpoints, and it responds to the users request. 
 // @route   GET api/users/register
 // @desc    Register user
 // @access  Public
-
 router.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
