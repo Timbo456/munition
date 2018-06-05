19b684ad47fa8798de3e587e6185a63fa371fd66
Timbo456 <samtaylor720@gmail.com>
2018-06-05 17:10:26 -0400

    Ton of stuff done. Finished up profile, website/social links, GitHub api, 404 page.

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_LOADING,
   CLEAR_CURRNET_PROFILE,
   GET_ERRORS,
@@ -27,6 +28,25 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
@@ -36,6 +56,95 @@ export const createProfile = (profileData, history) => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    );
+};
+
+// Add experience
+
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    //this is hitting the endpoint. the .then is the resoponse redirect to the dashboard. .catch is for errors
+    .post("/api/profile/experience", expData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add education
+
+export const addEducation = (eduData, history) => dispatch => {
+  axios
+    //this is hitting the endpoint. the .then is the resoponse redirect to the dashboard. .catch is for errors
+    .post("/api/profile/education", eduData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete Experience (for use with button on exp page)
+export const deleteExperience = id => dispatch => {
+  axios
+    //This is hitting the endpoint. The backtick (`) instead of quote (") is so we can use a template litteral, passing the variable for the id.
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete Education
+export const deleteEducation = id => dispatch => {
+  axios
+    //This is hitting the endpoint. The backtick (`) instead of quote (") is so we can use a template litteral, passing the variable for the id.
+    .delete(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Get all profiles for developers
+
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    //This is hitting the endpoint.
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     );
 };
