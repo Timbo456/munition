55fabf6be05ac36f6d3a2c270be1347c829332b8
Timbo456 <samtaylor720@gmail.com>
2018-06-12 17:58:41 -0400

    Google/FB auth working, got SSL cert

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 export default () => {
   return (
     // can add fixed-bottom to bootstrap to hold to bottom
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} The Munition (a ST joint!)
+    <footer className="fixed-bottom bg-dark text-grey mt-5 p-2 text-center">
+      Copyright &copy; {new Date().getFullYear()} The Munition (an ST joint!)
     </footer>
   );
 };
