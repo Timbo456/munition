22059192d772f42c7e341574333c4fda7bfb9701
Timbo456 <samtaylor720@gmail.com>
2018-06-12 13:39:01 -0400

    Changed footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ export default () => {
   return (
     // can add fixed-bottom to bootstrap to hold to bottom
     <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} DevConnector
+      Copyright &copy; {new Date().getFullYear()} The Munition (a ST joint!)
     </footer>
   );
 };
